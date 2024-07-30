4c48f2eb95961132dcf2916119ca3ec5401518d2
kadelcode <kadelcode@gmail.com>
2024-07-30 21:56:58 +0100

    Update settings for template and s

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'edubridge_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Add project-level templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # URL prefix for static files
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Optional for deployment
+
+STATICFILES_DIRS = [
+        BASE_DIR / 'static', # Project-level static files
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
