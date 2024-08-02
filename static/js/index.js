8ef3911736f979e67d56f7d74e58cdaefb0b4069
kadelcode <kadelcode@gmail.com>
2024-08-02 22:06:08 +0100

    Add structure, styles, and behavior to the navigation section

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Menu icon button
     const menuIcon = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav ul');
 
-    const selectButton = document.querySelector('.select-button');
-    const chevron = selectButton.querySelector('.chevron-down');
+    // Nav Div
+    const nav = document.querySelector('nav');
 
-    selectButton.addEventListener('click', () => {
-        chevron.classList.toggle('chevron-up')
-    })
+    // Dropdown button
+    const dropDownButton = document.querySelector('.dropdown-button');
+    // Dropdown contents
+    const dropDownContent = document.querySelector('.dropdown-content');
 
+    // Chevron down button
+    const chevron = dropDownButton.querySelector('.chevron-down');
 
+    // Dropdown behavior
+    dropDownButton.addEventListener('click', () => {
+        chevron.classList.toggle('chevron-up');
+        dropDownContent.classList.toggle('show');
+    });
+
+    // Menu icon behavior
     menuIcon.addEventListener('click', function() {
-	nav.classList.toggle('active');
+	    nav.classList.toggle('active');
+        menuIcon.classList.toggle('rotate-hamburger')
     });
 });
