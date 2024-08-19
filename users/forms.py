d8d7adc0f395479dddc4a32f3d8951d1df2d72ab
kadelcode <kadelcode@gmail.com>
2024-08-19 13:23:15 +0100

    Add custom user creation form

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class CustomUserCreationForm(forms.Form):
+    fullname = forms.CharField(max_length=200, required=True)
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+
+        return cleaned_data
