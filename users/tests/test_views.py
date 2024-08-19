c5a5414730ea1e9e1b7ab6894bc577e0bd557193
kadelcode <kadelcode@gmail.com>
2024-08-19 18:35:07 +0100

    Add new unit test case

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -10,3 +10,7 @@ class SignupViewTest(TestCase):
     def test_signup_view_template(self):
         response = self.client.get(reverse('signup'))
         self.assertTemplateUsed(response, 'users/signup.html')
+
+    def test_signup_page_contains_correct_html(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Create Account')
