d54218a454d491d80ecf1d5f272d80870c8aec14
kadelcode <kadelcode@gmail.com>
2024-08-22 14:40:38 +0100

    Add custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm
 from .models import CustomUser
 
+class CustomerUserAdmin(UserAdmin):
+    form = CustomUserCreationForm
+    #form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'fullname', 'is_staff', ] 
+
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomerUserAdmin)
