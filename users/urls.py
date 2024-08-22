a46e2c0c08cd8753140b148b99fea2a329f8a7bc
kadelcode <kadelcode@gmail.com>
2024-08-22 16:19:03 +0100

    Update login and signup url path

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.signup, name='signup')
+    path('register/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login')
 ]
