9a520bef62412e5b998f982a5874ea454d2fcb0f
kadelcode <kadelcode@gmail.com>
2024-08-22 16:15:41 +0100

    Update custom user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True) 
+
+    # Other fields (e.g., first_name, last_name)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    #username = models.CharField(max_length=150, unique=True, blank=True)
     fullname = models.CharField(max_length=200)
