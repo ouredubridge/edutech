591d8593ff00721136f2f84fc5e3304731b41a1b
kadelcode <kadelcode@gmail.com>
2024-08-23 22:30:01 +0100

    Update code to use email for authentication and not username

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,34 @@ from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 class CustomerUserAdmin(UserAdmin):
-    form = CustomUserCreationForm
+    #form = CustomUserCreationForm
     #form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'fullname', 'is_staff', ] 
+
+    # Ensure search_fields use fields that exist in your CustomUser model
+    search_fields = ('email', 'first_name', 'last_name')
+
+    ordering = ['email']
+    list_display = ['email', 'fullname', 'is_staff', ]
+
+    #list_display = ['email', 'fullname', 'is_staff', ] 
+
+    # Update fieldsets to remove references to username
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        #('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    # Also, if you have any add_fieldsets, update them as well
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 
 # Register your models here.
 admin.site.register(CustomUser, CustomerUserAdmin)
+#admin.site.register(CustomUser)
