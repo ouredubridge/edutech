204021a54e83ba81e2e2fea3f7be3d8715947ecb
kadelcode <kadelcode@gmail.com>
2024-08-24 07:59:12 +0100

    Add paths for login and logout views

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
