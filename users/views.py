53134a182b333b534fc6fc350759dc01c9ba992c
kadelcode <kadelcode@gmail.com>
2024-08-24 07:58:06 +0100

    Update code logic for login view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import CustomUserCreationForm, CustomUserLoginForm
 from .models import CustomUser
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def signup(request):
@@ -44,4 +47,30 @@ request -- The request object
 Return: The login template render
 """
 def login_view(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = CustomUserLoginForm(request.POST)
+
+        if form.is_valid():
+            #email = form.cleaned_data['email']
+            #password = form.cleaned_data['password']
+
+            # Authenticate user
+            user = form.get_user()
+
+            if user is not None:
+                # login user
+                login(request, user)
+                return redirect('home')
+
+        #else:
+            #context = {'form': form, 'error_message': 'Invalid login credentials'}
+            #return render(request, 'users/login.html', context)
+
+    else:
+        form = CustomUserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully!')
+    return redirect('home')
