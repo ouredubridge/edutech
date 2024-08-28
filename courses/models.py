791589af54f555b72d6d48d16dc1007544a42338
kadelcode <kadelcode@gmail.com>
2024-08-28 08:04:14 +0100

    Course model test

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from users.models import CustomUser
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    instructor = models.CharField(max_length=100)
+    published_data = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(CustomUser, related_name='enrolled_courses')
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Subsection(models.Model):
+    name = models.CharField(max_length=100)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    content = models.TextField()
+    order = models.IntegerField()
+
+class Enrollment(models.Model):
+    choices=[
+        ('enrolled', 'Enrolled'),
+        ('completed', 'Completed'),
+        ('dropped', 'Dropped')
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, default="Not Enrolled", choices=choices)
+    progress = models.IntegerField(default=0)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
