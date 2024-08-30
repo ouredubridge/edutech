50f07fe4c665d36119e622b6e6a6231686b8c6fc
kadelcode <kadelcode@gmail.com>
2024-08-30 18:04:42 +0100

    Add new test case

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -25,4 +25,11 @@ class HomePageTests(TestCase):
         response = self.client.get(reverse('home'))
         self.assertNotContains(response, 'Hi Kadel')
 
+    # Verifies that the contact us form is found in the homepage
+    def test_homepage_contains_contact_us_form(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Contact Us')
+        self.assertContains(response, 'form')
+        self.assertContains(response, 'input')
+
     # Ensures that the view passes the correct context data to the template
