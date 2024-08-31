746dd1e7afc300503033a6ac28de68fbf10866c9
kadelcode <kadelcode@gmail.com>
2024-08-31 17:02:24 +0100

    Update file

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-azj8xrhwe!ai8spi1dol+t_*nk93g(la^ugy7gt*qf%jo8%4^%'
+# SECRET_KEY = os.environ.get('EDUBRIGE_SECRET_KEY')
+SECRET_KEY = os.getenv('EDUBRIGE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,3 +148,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'login'
+
+
+# Email configuration settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kadelcode@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EDUBRIGE_EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = 'kadelcode@gmail.com'  # Replace with your default from email address
