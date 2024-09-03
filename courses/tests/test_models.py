a706754453f7c327d8075e90c055f4f1fabb6189
kadelcode <kadelcode@gmail.com>
2024-09-03 21:18:37 +0100

    Test models

--- courses/tests/test_models.py
+++ courses/tests/test_models.py
@@ -1,16 +1,6 @@
 from django.test import TestCase
-from courses.models import Course, Category#, Enrollment
+from courses.models import Category, Course, Module, Lesson#, Enrollment
 from users.models import CustomUser
-
-class CourseModelTests(TestCase):
-    def test_course_creation(self):
-        course = Course.objects.create(
-            title="Introduction to Python",
-            category=Category.objects.create(name="Programming", description="Learn how to program"),
-            description="Learn the basics of Python",
-            instructor=CustomUser.objects.create(fullname="Test User", email="testuser@gmail.com")
-        )
-        self.assertEqual(course.title, "Introduction to Python")
 
 class CategoryModelTests(TestCase):
     def test_category_creation(self):
@@ -20,3 +10,38 @@ class CategoryModelTests(TestCase):
         )
 
         self.assertEqual(category.description, "Become a professional in coding")
+
+
+class CourseModelTests(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title="Learn Python",
+            category=Category.objects.create(name="Programming", description="Learn how to program"),
+            description="Learn the basics of Python",
+            instructor=CustomUser.objects.create(fullname="Test User", email="testuser@gmail.com")
+        )
+        self.assertEqual(course.title, "Learn Python")
+
+class ModuleModelTests(TestCase):
+    def test_module_creation(self):
+        module = Module.objects.create(
+            course = Course.objects.create(
+                title="Learn Python",
+                category=Category.objects.create(name="Programming", description="Learn how to program"),
+                description="Learn the basics of Python",
+                instructor=CustomUser.objects.create(fullname="Test User", email="testuser@gmail.com")
+            ),
+            title="Introduction to Python",
+            description="Have an overview of what Python is all about",
+            order=1,
+        )
+        self.assertEqual(module.title, "Introduction to Python")
+
+class LessonModelTests(TestCase):
+    def test_lesson_creation(self):
+        lesson = Lesson.objects.create(
+            title="Variables and Datatypes",
+            content="Variables are containers for values",
+            order=1,
+        )
+        self.assertEqual(lesson.title, "Variables and Datatypes")
