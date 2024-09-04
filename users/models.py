3f8c6d98a05360e3d5f500760e5cf5e15952b968
kadelcode <kadelcode@gmail.com>
2024-09-04 13:29:11 +0100

    Add new field

--- users/models.py
+++ users/models.py
@@ -38,6 +38,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     # Other fields (e.g., first_name, last_name)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_instructor = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = CustomUserManager()
