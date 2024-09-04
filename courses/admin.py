b00d8fa58294b1bc51e4e0ad329adbc95de04495
kadelcode <kadelcode@gmail.com>
2024-09-04 13:05:48 +0100

    Register Module model

--- courses/admin.py
+++ courses/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Category, Course, Lesson, Subsection, Enrollment
+from .models import Category, Course, Module, Lesson, Enrollment
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Course)
+admin.site.register(Module)
 admin.site.register(Lesson)
-admin.site.register(Subsection)
+
 admin.site.register(Enrollment)
