24d5d474375c7afaeb841267b0cd602fd9076151
kadelcode <kadelcode@gmail.com>
2024-09-07 21:22:08 +0100

    Add urls for ckeditor and tinymce

--- edubridge_website/urls.py
+++ edubridge_website/urls.py
@@ -17,9 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('users.urls')),
     path('', include('courses.urls')),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
