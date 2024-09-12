e2cf5152e70768f13a5e2c9984df33ad86164ccf
kadelcode <kadelcode@gmail.com>
2024-09-12 20:36:27 +0100

    Add tests for valid email

--- users/tests/test_functional.py
+++ users/tests/test_functional.py
@@ -117,3 +117,16 @@ class PasswordResetInvalidEmailTests(TestCase):
         self.assertEqual(response.status_code, 302) # Should redirect to password reset done
         self.assertRedirects(response, reverse('password_reset_done'))
         self.assertEqual(len(mail.outbox), 0) # No email should be sent
+
+
+class PasswordResetValidEmailTests(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(fullname="testuser newuser", email='testuser@gmail.com', password='testuser123')
+
+    def test_valid_email_submission(self):
+        response = self.client.post(reverse('password_reset'), {'email': self.user.email})
+
+        self.assertEqual(response.status_code, 302) # Should redirect to password reset done
+        self.assertRedirects(response, reverse('password_reset_done'))
+        self.assertEqual(len(mail.outbox), 1) # One(1) email should be sent
