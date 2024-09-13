8daad55e338a2627d6bc04653dbbed68a40700cd
kadelcode <kadelcode@gmail.com>
2024-09-13 20:06:53 +0100

    Password reset email tests

--- users/tests/test_integration.py
+++ users/tests/test_integration.py
@@ -5,6 +5,10 @@ from users.models import CustomUser
 from django.urls import reverse
 from django.core import mail
 
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+
 class PasswordResetInvalidEmailTests(TestCase):
 
     def test_invalid_email_submission(self):
@@ -25,3 +29,23 @@ class PasswordResetValidEmailTests(TestCase):
         self.assertEqual(response.status_code, 302) # Should redirect to password reset done
         self.assertRedirects(response, reverse('password_reset_done'))
         self.assertEqual(len(mail.outbox), 1) # One(1) email should be sent
+
+class PasswordResetEmailTests(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(fullname="Test NewUser", email='test@example.com', password="oldpassword")
+
+    def test_password_reset_email_contains_correct_link(self):
+        # Trigger password reset email
+        self.client.post(reverse('password_reset'), {'email': self.user.email})
+
+        # Get the email sent
+        email = mail.outbox[0]
+
+        # Check the subject and recipient
+        self.assertIn('Password reset', email.subject)
+        self.assertIn(self.user.email, email.to)
+
+        # Check that the reset link contains the uid64 and token
+        uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
+        self.assertIn(f'/reset/{uidb64}/', email.body)
