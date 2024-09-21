323371a79e7a41930faf02e24455cfec0785dae3
kadelcode <kadelcode@gmail.com>
2024-09-21 15:53:20 +0100

    payments urls

--- edubridge_website/urls.py
+++ edubridge_website/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('users.urls')),
     path('', include('courses.urls')),
+    path('', include('payments.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
