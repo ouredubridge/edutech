7399556710fb29ec8defab5d2c9c515bdee31395
kadelcode <kadelcode@gmail.com>
2024-09-27 19:24:14 +0100

    Add is_ajax method to determine if request is an ajax request

--- community/views.py
+++ community/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# AJAX Request
+def is_ajax(request):
+    return request.headers.get('x-requested-with') == 'XMLHttpRequest'
+
 def community(request):
+    if is_ajax(request):
+        return render(request, 'community/partials/community_content.html')
     return render(request, 'community/community.html')
