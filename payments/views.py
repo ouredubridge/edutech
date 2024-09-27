42434fdac5ea24d4c4be06dfcb26b1a30b11b168
kadelcode <kadelcode@gmail.com>
2024-09-27 19:36:18 +0100

    Update urls

--- payments/views.py
+++ payments/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def pricing_page(request):
-    return render(request, 'pricing.html')
+    return render(request, 'payments/pricing.html')
