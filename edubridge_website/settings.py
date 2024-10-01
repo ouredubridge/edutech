e602743e803b010d9ca84b85770df7d5eb3b6862
kadelcode <kadelcode@gmail.com>
2024-10-01 21:30:28 +0100

    PostgreSQL integration

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -96,8 +96,14 @@ WSGI_APPLICATION = 'edubridge_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edubridge_db',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
