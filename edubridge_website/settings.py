8ca229a4e5a15731b5c263b072e820e730691d25
kadelcode <kadelcode@gmail.com>
2024-10-02 09:00:28 +0100

    Update code

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -98,12 +98,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        """'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'edubridge_db',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '35.160.120.126',
-        'PORT': '5432',"""
     }
 }
 
