075f2dc4e124d122c19f16f8a23f9a68fea0d01b
kadelcode <kadelcode@gmail.com>
2024-10-07 12:30:12 +0100

    Add create course url path

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('courses/', views.course_list, name='course_list'),
     path('courses/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('create-course/', views.create_course, name='create_course'),
 ]
