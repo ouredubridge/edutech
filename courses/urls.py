74406b7c0182a30b9aa5e226e4da869f3813bcac
kadelcode <kadelcode@gmail.com>
2024-10-12 15:23:18 +0100

    Edit course path

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('courses/<int:course_id>/', views.course_detail, name='course_detail'),
     path('create-course/', views.create_course, name='create_course'),
     path('course/<int:course_id>/create-module/', views.create_module, name='create_module'),
-    path('module/<int:module_id>/create-lesson/', views.create_lesson, name='create_lesson'),  
+    path('module/<int:module_id>/create-lesson/', views.create_lesson, name='create_lesson'),
+    path('course/<int:course_id>/edit/', views.edit_course, name='edit_course'),
 ]
