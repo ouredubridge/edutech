ccbc1b8fb056444b14a158e60e6f6f6104dba486
kadelcode <kadelcode@gmail.com>
2024-10-25 12:33:37 +0100

    Add new fields to model

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 from tinymce.models import HTMLField
 
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -24,12 +25,29 @@ class Course(models.Model):
         (SUBSCRIPTION, 'Subscription')
     ]
 
+    LEVEL_CHOICES = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced')
+    ]
+
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #image field
+    image = models.ImageField(
+        upload_to='course_images/',
+        null=True,
+        blank=True,
+        default='course_images/default_course_image.jpeg')
+
     instructor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='courses')
     published_date = models.DateTimeField(auto_now_add=True)
     students = models.ManyToManyField(CustomUser, related_name='enrolled_courses')
+
+    level = models.CharField(max_length=20, choices=LEVEL_CHOICES, default='beginner')
+    duration = models.PositiveIntegerField(help_text="Duration in hours")
 
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default=FREE)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
@@ -40,6 +58,12 @@ class Course(models.Model):
     
     #is_active = models.BooleanField(default=True)
     #is_paid = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super(Course, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
