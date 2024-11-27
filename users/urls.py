d8f3c69789844b61d3a5bab9f89eab38c2a8c605
kadelcode <kadelcode@gmail.com>
2024-11-27 15:58:09 +0100

    Add delete account url

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('delete-account/', views.delete_account, name='delete-account'),
 ]
