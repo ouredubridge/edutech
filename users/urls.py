331627bcb7a089303b665c4177b468889ea5f606
kadelcode <kadelcode@gmail.com>
2024-12-03 13:30:55 +0100

    Add fdd url

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('delete-account/', views.delete_account, name='delete-account'),
+    path('facebook/data-deletion/', views.facebook_data_deletion, name='facebook-data-deletion')
 ]
