daae58d8fae90941c6ada8331eca811baf3fad84
kadelcode <kadelcode@gmail.com>
2024-12-03 14:39:12 +0100

    Update url

--- legal/urls.py
+++ legal/urls.py
@@ -5,6 +5,6 @@ from .views import terms_of_services_page
 
 urlpatterns = [
     path('cookies_policy', cookies_policy_page, name='cookies_policy'),
-    path('privacy_polcy', privacy_policy_page, name='privacy_policy'),
+    path('privacy_policy', privacy_policy_page, name='privacy_policy'),
     path('terms_of_services', terms_of_services_page, name='terms_of_services'),
 ]
