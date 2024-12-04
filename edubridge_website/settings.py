bff77e5bd4161b8886fd294e297b922a1fbc15a1
kadelcode <kadelcode@gmail.com>
2024-12-04 15:07:45 +0100

    Add db url for render

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 import os
 from dotenv import load_dotenv
@@ -98,10 +99,13 @@ WSGI_APPLICATION = 'edubridge_website.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    """'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    }"""
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
