2db4049bcb4e365495d50ce2df41fafbc6132bde
kadelcode <kadelcode@gmail.com>
2024-12-08 18:41:14 +0100

    Add static & media context processors

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -87,6 +87,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #New
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
             ],
         },
     },
