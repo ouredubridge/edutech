f000b7279becffd04f25ab203c2c8fe679b9fa5d
kadelcode <kadelcode@gmail.com>
2024-12-28 16:00:03 +0100

    Group and Membership models

--- community/models.py
+++ community/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
