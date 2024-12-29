0e36c051cdc410f5c18f219a7c96b2073035ae9f
kadelcode <kadelcode@gmail.com>
2024-12-29 19:18:37 +0100

    Membership model

--- community/models.py
+++ community/models.py
@@ -9,3 +9,18 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
+class Membership(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('moderator', 'Moderator'),
+        ('memeber', 'Member'),
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='memberships')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='member')
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'group')
