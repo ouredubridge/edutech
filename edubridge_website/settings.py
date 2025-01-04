a9c960afb5564c97651a59b97c01a1fa23d15131
kadelcode <kadelcode@gmail.com>
2025-01-04 14:22:29 +0100

    Migration file

--- edubridge_website/settings.py
+++ edubridge_website/settings.py
@@ -102,10 +102,10 @@ WSGI_APPLICATION = 'edubridge_website.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        #default=f"sqlite:///db.sqlite3"
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }   
 }
 
 
