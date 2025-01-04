51b214fbd56f0618163f0e26c2364e5c60297e9d
kadelcode <kadelcode@gmail.com>
2025-01-04 15:29:03 +0100

    Code fix

--- community/tests/test_views.py
+++ community/tests/test_views.py
@@ -5,15 +5,19 @@ from community.models import Group, Membership, Post
 
 
 class CommunityViewTest(TestCase):
-    # Create a user and log them in
-    self.user = CustomUser.objects.create_user(
-        fullname = 'Test Newuser',
-        username='testuser',
-        password='testpassword'
-    )
+    def setUp(self):
+        # Create a user and log them in
+        self.user = CustomUser.objects.create_user(
+            fullname = 'Test Newuser',
+            email='testuser@gmail.com',
+            password='testpassword'
+        )
 
-    # Create a group for the user
-    self.group = Group.objects.create(
-        name="Test Group",
-        description="A test group"
-    )
+        # Create a group for the user
+        self.group = Group.objects.create(
+            name="Test Group",
+            description="A test group"
+        )
+
+#def test_group_list_view(self):
+    #response = self.client.get(reverse('group_list'))
