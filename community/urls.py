42d5f7534ddb1a36fe44a74fae2c5684c96fdad5
kadelcode <kadelcode@gmail.com>
2025-01-06 17:54:10 +0100

    Add group list route

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('community/', views.community, name='community')
+    path('community/', views.community, name='community'),
+    path('group-list', views.group_list, name='group_list')
 ]
