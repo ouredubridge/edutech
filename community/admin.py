4776bff03bdafa6c317e69fd1c85a31407e4d963
kadelcode <kadelcode@gmail.com>
2025-01-09 20:01:30 +0100

    Setup the admin interface

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Group, Membership, Post, Comment
 
 # Register your models here.
+admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(Post)
+admin.site.register(Comment)
