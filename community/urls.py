b8fc6412892bedf10e72e262f95e8386244dd3db
kadelcode <kadelcode@gmail.com>
2025-01-11 16:52:04 +0100

    Update url

--- community/urls.py
+++ community/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('community/', views.community, name='community'),
     path('group-list', views.group_list, name='group_list'),
-    path('group/<int:group_id', views.group_detail, name='group_detail'),
+    path('group/<int:group_id>/', views.group_detail, name='group_detail'),
 ]
