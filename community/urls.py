29f80bed9b03c194f30b05282a27ded3c2a98301
kadelcode <kadelcode@gmail.com>
2025-01-11 16:50:32 +0100

    Add group detail url path

--- community/urls.py
+++ community/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('community/', views.community, name='community'),
-    path('group-list', views.group_list, name='group_list')
+    path('group-list', views.group_list, name='group_list'),
+    path('group/<int:group_id', views.group_detail, name='group_detail'),
 ]
