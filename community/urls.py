01fe54b3b7f737f0a53cce2dd90d2e50297cfcc4
kadelcode <kadelcode@gmail.com>
2025-01-18 16:12:39 +0100

    Add url path

--- community/urls.py
+++ community/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('group-list', views.group_list, name='group_list'),
     path('group/<int:group_id>/', views.group_detail, name='group_detail'),
     path('group/<int:group_id>/join/', views.join_group, name='join_group'),
+    path('group/<int:group_id>/create_post/', views.create_post, name='create_post'),
 ]
