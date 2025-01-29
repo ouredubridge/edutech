b23d5d3bf9fd4752195ad729575310c6449dbaca
kadelcode <kadelcode@gmail.com>
2025-01-29 18:42:05 +0100

    Permission test

---
+++ community/tests/test_permission.py
@@ -0,0 +1,11 @@
+from community.models import Group
+
+class CommunityPermissionTest(TestCase):
+    def setUp(self):
+        # Create a group
+        self.group = Group.objects.create(name="Test Group", description="A test group")
+
+    def test_unauthorized_access(self):
+        # Test group list (should redirect to login)
+        response = self.client.get(reverse('group_list'))
+        self.assertNotEqual(response.status_code, 200)
