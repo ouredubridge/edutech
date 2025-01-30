b6645ff4b3a3759c89b142626d661aaa7e92f578
kadelcode <kadelcode@gmail.com>
2025-01-30 16:29:51 +0100

    Update code

--- community/tests/test_permission.py
+++ community/tests/test_permission.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from community.models import Group
 
 class CommunityPermissionTest(TestCase):
@@ -9,4 +10,8 @@ class CommunityPermissionTest(TestCase):
     def test_unauthorized_access(self):
         # Test group list (should redirect to login)
         response = self.client.get(reverse('group_list'))
+        self.assertNotEqual(response.status_code, 200)
+
+        # Test group detail (should redirect to login)
+        response = self.client.get(reverse('group_detail', args=[self.group.d]))
         self.assertNotEqual(response.status_code, 200)
